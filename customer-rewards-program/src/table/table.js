33b0d6ccadba1c85be458e5ab754199ab3e9baa4
mspenski <44811974+mspenski@users.noreply.github.com>
2020-02-05 22:45:10 -0500

    initial rough draft of table rendering

--- customer-rewards-program/src/table/table.js
+++ customer-rewards-program/src/table/table.js
@@ -1,61 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col, Table } from 'react-bootstrap';
 import './table.css';
 
 
-class CustomerTable extends Component {
-  constructor(props) {
-    super(props);
+const CustomerTable = props => {
 
-    let tableRow = []
+  let tableRow = []
 
-    createNewRow = () => {
-      for (let i = 0; i < 6; i++) {
-        let data = Math.floor(Math.random() * 200);
-        this.tableRow.push(data);
-      }
-    }
-
-    renderTableRows = () => {
-      this.createNewRow();
-      return (
-        <tr>
-          <td>{tableRow[0]}</td>
-          <td>{tableRow[1]}</td>
-          <td>{tableRow[2]}</td>
-          <td>{tableRow[3]}</td>
-          <td>{tableRow[4]}</td>
-          <td>{tableRow[5]}</td>
-        </tr>
-      )
+  const createNewRow = () => {
+    for (let i = 0; i < 6; i++) {
+      let data = Math.floor(Math.random() * 200);
+      tableRow.push(data);
     }
   }
 
-  render() {
+  const renderTableRows = () => {
+    createNewRow();
     return (
-      <>
-        < Row >
-          <Col>
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>Customer 1</th>
-                  <th>Customer 2</th>
-                  <th>Customer 3</th>
-                  <th>Customer 4</th>
-                  <th>Customer 5</th>
-                  <th>Customer 6</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.renderTableRows()}
-              </tbody>
-            </Table>
-          </Col>
-        </Row >
-      </>
+      <tr>
+        <td>{tableRow[0]}</td>
+        <td>{tableRow[1]}</td>
+        <td>{tableRow[2]}</td>
+        <td>{tableRow[3]}</td>
+        <td>{tableRow[4]}</td>
+        <td>{tableRow[5]}</td>
+      </tr>
     )
   }
+
+  return (
+    <>
+      < Row >
+        <Col>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>Customer 1</th>
+                <th>Customer 2</th>
+                <th>Customer 3</th>
+                <th>Customer 4</th>
+                <th>Customer 5</th>
+                <th>Customer 6</th>
+              </tr>
+            </thead>
+            <tbody>
+              {renderTableRows()}
+            </tbody>
+          </Table>
+        </Col>
+      </Row >
+    </>
+  )
 }
+
 
 export default CustomerTable;
